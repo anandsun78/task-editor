9c5b44ca0d07fee4148e64066f3a386faf0a0abc
anandsun78 <anandsun78@gmail.com>
2020-11-29 14:40:34 -0700

    Add initial dashboard

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+import React from "react";
+import ReactDom from "react-dom";
+import Main from "../components/Main";
+
+ReactDom.render(<Main />, document.getElementById("app"));
